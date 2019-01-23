714af3e9026823c57bef66169a69f49d737a477d
HeechanYang <yhc944@gmail.com>
2019-01-24 00:22:16 +0900

    [ADD] Baekjoon 6549. BiggestSquare Using `Devide and Conquer`. Close #14

--- src/baekjoon/P6549_BiggestSquare.java
+++ src/baekjoon/P6549_BiggestSquare.java
@@ -9,52 +9,51 @@ import java.util.StringTokenizer;
  * https://www.acmicpc.net/problem/6549
  */
 public class P6549_BiggestSquare {
-    private static int N;
-    private static int[] heightArr;
 
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
             while (true) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                N = Integer.parseInt(st.nextToken());
+                int N = Integer.parseInt(st.nextToken());
+
                 if (N == 0) {
-                    return;
-                }
-                heightArr = new int[N];
+                    System.out.println(sb);
+                    break;
+                } else {
+                    int[] heightArr = new int[N];
+                    long maxHeight = 0;
 
-                for (int i = 0; i < N; i++) {
-                    heightArr[i] = Integer.parseInt(st.nextToken());
-                }
-
-                long maxArea = 0;
-
-                for (int i = 0; i < N; i++) {
-                    int thisHeight = heightArr[i];
-                    long thisArea = 0;
-
-                    //왼쪽으로
-                    for (int j = i; j >= 0; j--) {
-                        int tempHeight = heightArr[j];
-                        if (tempHeight >= thisHeight) {
-                            thisArea += thisHeight;
-                        } else {
-                            break;
-                        }
-                    }
-                    // 오른쪽으로
-                    for (int j = i + 1; j < N; j++) {
-                        int tempHeight = heightArr[j];
-                        if (tempHeight >= thisHeight) {
-                            thisArea += thisHeight;
-                        } else {
-                            break;
-                        }
+                    for (int i = 0; i < N; i++) {
+                        heightArr[i] = Integer.parseInt(st.nextToken());
+                        maxHeight = Math.max(maxHeight, heightArr[i]);
                     }
 
-                    maxArea = Math.max(maxArea, thisArea);
-                }
+                    long maxArea = 0;
 
-                System.out.println(maxArea);
+                    for (int i = 0; i < N; i++) {
+                        long thisHeight = heightArr[i];
+                        long tempMaxArea = N * thisHeight;
+
+                        if (tempMaxArea < maxHeight
+                                || tempMaxArea < maxArea) {
+                            continue;
+                        }
+
+                        int l, r;
+                        //왼쪽으로
+                        for (l = i; l > -1 && (heightArr[l] >= thisHeight); l--) ;
+                        // 오른쪽으로
+                        for (r = i; r < N && (heightArr[r] >= thisHeight); r++) ;
+
+                        int width = r - l - 1;
+                        long thisArea = width * thisHeight;
+
+                        maxArea = Math.max(maxArea, thisArea);
+                    }
+
+                    sb.append(maxArea).append('\n');
+                }
             }
         }
     }
