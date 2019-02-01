0947310276382f3c223ee97660783c22ddf7131d
HeechanYang <yhc944@gmail.com>
2019-02-01 21:06:58 +0900

    [ADD] Solving for Codeforces Round #535 (Div. 3). Problem B. Closes #17.

--- src/codeforces/Problem2.java
+++ src/codeforces/Problem2.java
@@ -4,6 +4,25 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * https://codeforces.com/contest/1108/problem/B
+ * B. Divisors of Two Integers
+ * <p>
+ * 한 양의 정수 배열(arr)이 주어지면, 그 배열 안의 요소들을 약수로 갖는 두 양의 정수(A, B)를 출력하는 문제
+ * - 양의 정수 배열은 정렬되어있지 않아도 됨
+ * - arr = A의 약수들 + B의 약수들
+ * ex) [1,2,4,1,2,3,6]  ->  4(1, 2, 4), 6(1, 2, 3, 6)
+ * <p>
+ * [해결방법]
+ * - 일단 A(or B)는 그 배열의 가장 큰 수이다.
+ * - 그렇다면 B(or A)는 그 배열에서 B의 약수들을 뺀 배열에서 가장 큰 수이다.
+ * - 뒤에서부터 지우고, 안지워진 첫 수가 B!
+ * - 정렬만 하면 됨.
+ * <p>
+ * Arrays.sort(int[] arr) 사용. DualPivotQuicksort!
+ * O(NlogN)
+ */
+
 public class Problem2 {
     private static int N;
     private static int A, B;
